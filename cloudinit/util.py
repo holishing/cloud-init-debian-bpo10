84873138311f360aa90976fc75a9ba97ea9ff74c
Thomas Goirand <zigo@debian.org>
2018-09-17 14:30:25 +0200

    Fix diff with upstream.

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -1212,7 +1212,7 @@ def find_devs_with(criteria=None, oformat='device',
       LABEL=<label>
       UUID=<uuid>
     """
-    blk_id_cmd = ['/sbin/blkid']
+    blk_id_cmd = ['blkid']
     options = []
     if criteria:
         # Search for block devices with tokens named NAME that
